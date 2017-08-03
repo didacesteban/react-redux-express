0f52389522ed653f54eb40c14413cf641ca67d54
Didac Esteban Andres <didacesteban@gmail.com>
2017-08-03 22:08:20 +0200

    added store

--- public/app.js
+++ public/app.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {createStore} from 'redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
 
 render(
   <div>
